726a6243489073602efa17f794e0ecea42cac5df
byron <jujy42@gmail.com>
2026-05-20 22:12:37 -0500

    created resume card and added puter setup files

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,12 +11,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    {/* {window.puter.ai.chat()} */}
+
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))} 
+        </div>
+      )}
     </section>
-  </main>;
+  </main>
 }
